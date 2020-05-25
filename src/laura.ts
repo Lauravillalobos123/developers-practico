7b5d47fbd9a3d3a4f33d91b5fce600ab53adde92
Laura Villalobos <lauravillalobossilva@gmail.com>
2020-05-25 14:46:49 -0500

    Resta en el archivo laura.ts

--- src/laura.ts
+++ src/laura.ts
@@ -4,6 +4,12 @@ console.log(cursos);
 function suma (a:number, b:number){
     return a + b;
 }
+//Funciones
+
+function resta(a:number, b:number){
+    return a-b;
+}
+let operacion = resta(5,2);
 
 const resultado = suma(20,30);
 console.log(resultado);
@@ -11,3 +17,4 @@ console.log(resultado);
 let curso = 'Typescript'
 let saludo = `Hola bienvenodos al curso de ${curso}`
 console.log(saludo);
+
